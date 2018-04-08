0b907e13dad6ef275685c6f6194322adb1226ff3
shadskii <jake.k.hassel@gmail.com>
2018-04-08 01:48:36 -0400

    Cant erase used tiles

--- src/reducers/tiles.js
+++ src/reducers/tiles.js
@@ -16,7 +16,7 @@ export default function tiles(state = initialState, action) {
     switch (action.type) {
         case types.UPDATE_TILE:
             return state.map(tile => {
-                if (tile.i === action.index) {
+                if (tile.i === action.index && tile.value === NONE) {
                     return {
                         i: tile.i,
                         value: action.value
