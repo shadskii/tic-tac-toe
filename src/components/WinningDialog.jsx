7f8e87c3d873af5a4c2aa01659bc2fce4e69a221
shadskii <jake.k.hassel@gmail.com>
2018-04-09 00:05:06 -0400

    Title to dialog

--- src/components/WinningDialog.jsx
+++ src/components/WinningDialog.jsx
@@ -6,6 +6,7 @@ import { O, X, NONE } from '../constants/Values';
 
 const WinningDialog = ({ resetGame, winner }) => (
     <Dialog
+        title={"Player " + getWinnerName(winner) + " has won!"}
         actions={
             <FlatButton
                 label="Play Again"
@@ -17,5 +18,12 @@ const WinningDialog = ({ resetGame, winner }) => (
         open={winner !== NONE}
     />
 );
+function getWinnerName(winner) {
+    if (winner === O) {
+        return 'O';
+    } else {
+        return 'X';
+    }
+}
 
 export default WinningDialog;
